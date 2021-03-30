a1a4e24abec5d6eebfe9e3b2c0e8fda176ff98f1
justEugene94 <drydude77@gmail.com>
2021-03-30 14:20:25 +0300

    Added script for adding and deleting columns

---
+++ src/data_generator/static/data_generator/add_and_delete_columns.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function () {
+  $('[type=button]').click(function (e) {
+    console.log('clicked')
+    let columns = $('.custom-row')
+    if ($(this).val() === 'add') {
+      let lastColumn = columns.last()
+      let clonedLastColumn = lastColumn.clone(true)
+      for (let elem of clonedLastColumn) {
+        for (let child of elem.children) {
+          if (child.tagName === 'LABEL') {
+            child.htmlFor = incrementedStringInFormsetElement(child.htmlFor)
+          } else if (child.tagName === 'INPUT' || child.tagName === 'SELECT') {
+            child.name = incrementedStringInFormsetElement(child.name)
+            child.id = incrementedStringInFormsetElement(child.id)
+          }
+        }
+      }
+      columns.last().after(clonedLastColumn)
+    } else {
+      console.log(columns.length)
+      if (columns.length > 1)
+        $(this).parent().remove()
+    }
+  })
+})
+
+function incrementedStringInFormsetElement (string) {
+  let array = string.split('-')
+  array[1] = parseInt(array[1]) + 1
+  return array.join('-')
+}
